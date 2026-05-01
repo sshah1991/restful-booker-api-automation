7ea137a6fdce67d2053e7f59955efe31c8822408
Sumeet Shah <sumeetshah.tsk@gmail.com>
2026-05-01 12:26:03 +0530

    code pust for TC:03

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "playwright/test";
 import { BaseController } from "../controllers/BaseController";
 import { AuthRequest, AuthResponse } from "../models/AuthModel";
 import { AuthController } from "../controllers/AuthController";
+import { request } from "playwright-extra";
 
 test('TC_002: Generate Auth Token Successfully @smoke', async ({ request }) => {
     const authController = new AuthController(request)
@@ -17,7 +18,7 @@ test('TC_002: Generate Auth Token Successfully @smoke', async ({ request }) => {
 
     // 2. Validate Status
     expect(response.status()).toBe(200)
-    console.log("status code: ",response.status())
+    console.log("status code: ", response.status())
     //console.log(response)
 
     // 3. Parse JSON using the Response Model
@@ -27,5 +28,29 @@ test('TC_002: Generate Auth Token Successfully @smoke', async ({ request }) => {
     console.log("Token Fetched: ", token)
 
     expect(responseJSON.token).toBeDefined()
+
+})
+
+test.only('TC_003: Login Failure--Invalid credentials @Regression', async ({ request }) => {
+    const authController = new AuthController(request)
+
+    //define the credentials
+    const credentials: AuthRequest = {
+        username: "Dummy_Username",
+        password: "Dummy_Password"
+    }
+
+    //call the createToken()
+    const response = await authController.createToken(credentials)
+
+    //validate status code is 200
+    expect(response.status()).toBe(200)
+
+    //caputure the resoponse and make sure response message is "Bad Credentials"
+    const responseJSON = await response.json()
+    const message = responseJSON.reason
+    expect(message).toBe('Bad credentials')
+    console.log(message)
+
 
 })
