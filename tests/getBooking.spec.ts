15df5a14b26a979352f555dea781809ba0a9dad7
Sumeet Shah <sumeetshah.tsk@gmail.com>
2026-05-02 05:07:55 +0530

    Code Push for TC 007-01

--- tests/getBooking.spec.ts
+++ tests/getBooking.spec.ts
@@ -2,7 +2,7 @@ import { getAllBookings } from "../models/BookingModel";
 import { GetBookingController } from "../controllers/GetBookingController";
 import test, { expect } from "playwright/test";
 
-test.only('TC_007: List All Bookings Successfully @sanity',async({request})=>{
+test('TC_007: List All Bookings Successfully @sanity',async({request})=>{
     const getBookingController= new GetBookingController(request)
     const response= await getBookingController.getAllBooking()
     expect(response.status()).toBe(200)
